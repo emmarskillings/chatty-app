290dca5b12e45d2e102c6e46fa4785543098ef95
emmarskillings <emmarskillings@gmail.com>
2018-07-10 21:54:29 +0000

    new messages are added to chat on 'Enter'

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
 
-class Message extends Component {
-  render() {
-    return (
-      <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-      </footer>
-    );
+const ChatBar = (props) => {
+
+  const generateUniqueKey = function(){
+    const uniqueKey = Math.random().toString(36).replace('0.','').split('').slice(0,6).join('');
+    return uniqueKey;
   }
+
+  const sendMessage = function(event) {
+    if (event.key === 'Enter') {
+      // event.preventDefault();
+      const newMessage = {
+        id: generateUniqueKey(),
+        username: document.getElementById('username').value,
+        content: event.target.value
+      }
+      props.addMessage(newMessage);
+      event.target.value = '';
+    }
+    else {
+      return false;
+    }
+  }
+
+  return (
+    <footer className="chatbar">
+      <input id='username' className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={props.name} />
+      <input onKeyDown={sendMessage} className="chatbar-message" placeholder="Type a message and hit ENTER" />
+    </footer>
+  );
 }
-export default Message;
+export default ChatBar;
