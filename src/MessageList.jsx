e58f6de452508e6d39e7b92f8b3c76896cfdd17b
emmarskillings <emmarskillings@gmail.com>
2018-07-10 19:15:29 +0000

    messages passed as props

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,9 +3,14 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    const messages = this.props.messages;
+
+    const messageList = messages.map((message) =>
+      <Message username={message.username} content={message.content} />
+    )
     return (
       <main className="messages">
-        <Message />
+        {messageList}
         <div className="message system">
           Anonymous1 changed their name to nomnom.
         </div>
