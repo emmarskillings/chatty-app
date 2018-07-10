4dabe3044d6ef4739d5f1f32c028df82c12dda8c
emmarskillings <emmarskillings@gmail.com>
2018-07-10 17:54:19 +0000

    chatty app react layout and currentUser passed down using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // this is the *only* time you should assign directly to state:
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar name={this.state.currentUser.name}/>
+      </div>
     );
   }
 }
---
+++ src/ChatBar.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.name} />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    );
+  }
+}
+export default Message;
